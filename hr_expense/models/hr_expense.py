c6e0f243b459f1a5dd505dba1aee765b1032108c
hamsky13 <ilham.ius13@gmail.com>
2022-10-12 11:16:20 +0700

    menambahkan field bill ref di form dan list

---
+++ hr_expense/models/hr_expense.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+class HrExpense(models.Model):
+    _inherit = 'hr.expense'
+
+
+class HrExpenseSheet(models.Model):
+    _inherit = 'hr.expense.sheet'
+
+    ref = fields.Char(string='Bill Reference')
