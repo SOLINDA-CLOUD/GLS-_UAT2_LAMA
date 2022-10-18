e96717db209b92c8bb0e4c62c0e3a7deeacb1c07
hamsky13 <ilham.ius13@gmail.com>
2022-10-18 17:26:40 +0700

    tes2

--- sol_cost_sheet/wizard/rap_report.py
+++ sol_cost_sheet/wizard/rap_report.py
@@ -4,8 +4,17 @@ class RapReportWizard(models.TransientModel):
     _name = 'rap.report.wizard'
     _description = 'Rap Report Wizard'
     
-    project_ids = fields.Many2many('project.project', string='Project')
+
+    api.depends('project_ids')
+    def _compute_project_name(self):
+        name = ''
+        for project in self.project_ids:
+            name = name + project.name
+        return name
 
     def download_xlsx_report(self):
         template_report = 'sol_cost_sheet.action_rap_report'
         return self.env.ref(template_report).report_action(self)
+
+    project_ids = fields.Many2many('project.project', string='Project')
+    name = fields.Char(string='Name', compute= _compute_project_name)
