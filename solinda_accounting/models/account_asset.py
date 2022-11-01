5cadf4e5cceed9ef6e6354d485440530189712ca
hamsky13 <ilham.ius13@gmail.com>
2022-11-01 19:17:09 +0700

    delete field checkbox pause depreciation

--- solinda_accounting/models/account_asset.py
+++
@@ -1,8 +0,0 @@
-from email.policy import default
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-
-class AccountAsset(models.Model):
-    _inherit = 'account.asset'
-
-    pause_dep_check = fields.Boolean("Pause Depreciation", default=False)
